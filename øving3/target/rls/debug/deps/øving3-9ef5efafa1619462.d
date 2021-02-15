12fb232efeef75bf92c14547e33bb8393c02e828
olokken <ole.lokken1@gmail.com>
2021-02-15 01:25:39 +0100

    kjor

---
+++ øving3/target/rls/debug/deps/øving3-9ef5efafa1619462.d
@@ -0,0 +1,5 @@
+/Users/olelokken/DEV/Nettverksprogrammering/øving3/target/rls/debug/deps/øving3-9ef5efafa1619462.rmeta: src/main.rs
+
+/Users/olelokken/DEV/Nettverksprogrammering/øving3/target/rls/debug/deps/øving3-9ef5efafa1619462.d: src/main.rs
+
+src/main.rs:
---
+++ øving3/target/rls/debug/deps/øving3-c83027a4e8744f5c.d
@@ -0,0 +1,5 @@
+/Users/olelokken/DEV/Nettverksprogrammering/øving3/target/rls/debug/deps/øving3-c83027a4e8744f5c.rmeta: src/main.rs
+
+/Users/olelokken/DEV/Nettverksprogrammering/øving3/target/rls/debug/deps/øving3-c83027a4e8744f5c.d: src/main.rs
+
+src/main.rs:
